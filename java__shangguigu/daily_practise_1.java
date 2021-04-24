425dd4e01981e3477ce0b509d9b5ac0f42bbbd8f
caixiongjiang <1837278011@qq.com>
2021-04-24 23:57:40 +0800

    master

--- java__shangguigu/daily_practise_1.java
+++
@@ -1,8 +0,0 @@
-public class daily_practise_1{
-    public static void main(String[] args){
-        System.out.println("姓名：蔡雄江");
-        System.out.println(); 
-        System.out.println("性别：男"); 
-        System.out.println("家庭住址：北京"); 
-    }
-}
