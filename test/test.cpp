b0999c5df7a8ce5b4d61fcd0e6eb7d4ef5ad7920
caixiongjiang <1837278011@qq.com>
2021-09-06 16:00:24 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -1,29 +1,35 @@
-#include<stdio.h> 
-int main()
-{
-int i, j, m, n; 
-scanf("%d", &n);
-m = n/2;
-for (i = 1; i <= m+1; i++){
-  for(j=1;j<=m+1-i;j++) 
-{
-    printf (" ");
-  }     
-  for(j=1;j<=2*i-1;j+=2)
-{
-    printf ("*");
-  }     
-  printf ("\n");
+#include<iostream>
+
+using namespace std;
+
+//冒泡排序函数
+void bubbleSort(int * arr, int len){
+  for(int i = 0; i < len - 1; i++){
+    for(int j = 0; j < len - i - 1; j++){
+      if(arr[j] > arr[j + 1]){
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 }
-for (i = m; i >= 1; i--){
-  for (j=1;j<i;j++)
-{
-    printf (" ");
-  }     
-  for(j=2*i-1;j>0;j--) 
-{
-    printf ("*");
-  }     
-  printf ("\n");
+
+//打印数组
+void printArray(int * arr, int len){
+  for(int i = 0;i < len; i++){
+    cout << arr[i] << endl;
+  }
 }
+
+int main(){ 
+  //1.先创建一个数组
+  int arr[10] = {4,3,6,9,1,2,10,8,7,5};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  //2.创建函数，实现冒泡排序
+  bubbleSort(arr, len);
+  //3.打印排序后的数组
+  printArray(arr, len);
+  system("pause");
+  return 0;
 }
