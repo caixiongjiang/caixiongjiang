b2d31e2956892b54fea592dd491d849de0ed91a9
caixiongjiang <1837278011@qq.com>
2021-09-15 09:39:35 +0800

    master

--- matlab速成/数学建模算法/分支定界法.m
+++ matlab速成/数学建模算法/分支定界法.m
@@ -1,3 +1,38 @@
+function [newx,newfval,status,newbound] = branchbound(f,A,B,I,x,fval,bound,Aeq,Beq,lb,ub,e)
+
+% 分支定界法求解整数规划
+% f,A,B,Aeq,Beq,lb,ub与线性规划相同
+% I为整数限制变量的向量
+% x为初始解，fval为初始值
+
+options = optimset('display','off');
+[x0,fval0,status0]=linprog(f,A,B,Aeq,Beq,lb,ub,[],options);
+
+%递归中的最终退出条件
+%无解或者解比现有上界大则返回原解
+if status0 <= 0 || fval0 >= bound
+    newx = x;
+    newfval = fval;
+    newbound = bound;
+    status = status0;
+    return;
+end
+
+%是否为整数解,如果是整数解则返回
+intindex = find(abs(x0(I) - round(x0(I))) > e);
+if isempty(intindex) %判断是否为空值
+    newx(I) = round(x0(I));
+    newfval = fval0;
+    newbound = fval0;
+    status = 1;
+    return;
+end
+
+%当有非整可行解时，则进行分支求解
+%此时必定会有整数解或空解
+%找到第一个不满足整数要求的变量
+n = I(intindex(1));
+addA = zeros(1,length(f));
 addA(n) = 1;
 %构造第一个分支 x<=floor(x(n))
 A = [A;addA];
