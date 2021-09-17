47fae7d36ef82449e631bd07ebc26bdf26ceb6fc
caixiongjiang <1837278011@qq.com>
2021-09-17 14:48:05 +0800

    master

--- test/test2.cpp
+++ test/test2.cpp
@@ -2,73 +2,63 @@
 
 using namespace std;
 
-//构造函数的分类和调用
-//1.分类
-//按照参数分类 无参构造  (默认构造)和有参构造
-//按照类型分类 普通构造  拷贝构造
+//拷贝构造的调用时机
+
 class Person {
 public:
-	//构造函数
-	Person()
-	{
-		cout << "Person 的无参构造函数调用" << endl;
+	Person() {
+		cout << "Person 的默认构造函数调用" << endl;
 	}
-	
-	Person(int a)
-	{
-		age = a;
+	Person(int age) {
+		m_Age = age;
 		cout << "Person 的有参构造函数调用" << endl;
 	}
-	//拷贝构造函数
+
 	Person(const Person &p) {
-		//将传入的人身上的所有属性拷贝到我身上
-		age = p.age;
+		m_Age = p.m_Age;
 		cout << "Person 的拷贝构造函数调用" << endl;
 	}
 
-	//析构函数
 	~Person() {
 		cout << "Person 的析构函数调用" << endl;
 	}
 
-	int age;
+	int m_Age;
 };
 
-//2.调用
+
+//1.使用一个已经创建完毕的对象初始化一个新对象
 void test01() {
-	//1.括号法
-	Person p1;//默认构造函数的调用
-	Person p2(10);//调用有参构造函数
-	Person p3(p2);//拷贝构造函数
+	Person p1(20);
+	Person p2(p1);
+	cout << "p2的年龄为：" << p2.m_Age << endl;
+}
+//2.值传递的方式给函数参数传值
+void doWork(Person p) {
 	
-	//注意事项
-	//调用默认构造函数时候，不要加()	
-	//因为下面这一行代码，编译器会认为是一个函数的声明,不会认为在创建对象
-	//Person p1();
-
-
-
-	cout << "p2的年龄为：" << p2.age << endl;
-	cout << "p3的年龄为：" << p3.age << endl;
-	//2.显示法
-	Person p4;
-	Person p5 = Person(10);//有参构造
-	Person p6 = Person(p5);//拷贝构造
-
-	Person(10);//匿名对象 特点：当前行执行结束后，系统就立即回收掉匿名对象	
-	
-	//注意事项2
-	//不要利用拷贝构造函数来初始化匿名对象 编译器会认为Person(p3) 等价于 Person p3;
-
-	//3.隐式转化法
-	Person p7 = 10; //相当于写了Person p4 = Person(10);
-	Person p8 = p7;//拷贝构造
 }
 
+void test02() {
+	Person p3;
+	doWork(p3);
+
+}
+//3.用值的方式返回局部对象
+Person doWork2() {
+	Person p4;
+	cout << (int*)&p4 << endl;
+	return p4;
+}
+
+void test03() {
+	Person p = doWork2();
+	cout << (int*)&p << endl;
+}
 int main() {
 
 	test01();
-
+	test02();
+	test03();
 	system("pause");
 	return 0;
 }
