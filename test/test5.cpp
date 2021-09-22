94e1cf826ca24cebdd41aebbc78fd53e7488ef5a
caixiongjiang <1837278011@qq.com>
2021-09-22 10:38:28 +0800

    master

--- test/test5.cpp
+++ test/test5.cpp
@@ -2,26 +2,43 @@
 
 using namespace std;
 
-//打印数据的函数
-void showValue(const int& val) {
-	//val = 1000;
-	cout << "val = " << val << endl;
+class Person {
+public:
+	Person(int age) {
+		//this指针指向被调用的成员函数所属的对象
+		//（谁调用这个函数就指向谁）
+		this->age = age;
+	}
+
+	Person&	PersonAddAge(Person& p) {
+		this->age += p.age;
+		//this是指向p2的指针，而*this指向的就是p2这个对选哪个本体
+		return *this;
+	}
+
+	int age;
+};
+
+//1.解决名称冲突
+void test01() {
+	Person p1(18);
+	cout << "p1的年龄为：" << p1.age << endl; 
 }
 
+//2.返回对象本身用*this
+
+void test02() {
+	Person p1(10);
+	Person p2(10);
+	//链式编程思想
+	p2.PersonAddAge(p1).PersonAddAge(p1).PersonAddAge(p1);
+	cout << "p2的年龄为： " << p2.age << endl;
+}
 
 int main() {
 
-	//常量引用
-	//使用场景：用来修饰形参，防止误操作
-	int a = 10;
-	//int &ref = 10; 引用本身需要一个合法的内存空间，因此这行错误
-	//加上const之后，编译器将代码修改为 int  temp = 10；const int & ref = temp;
- 	const int& ref = 10;
-	//ref = 20;//加入const之后变为只读，不可以修改
-
-	int b = 100;
-	showValue(b);
-
+	test01();
+	test02();
 	system("pause");
 	return 0;
 }
