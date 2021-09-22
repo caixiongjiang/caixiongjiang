197f1de30daccd99cd0910f7ebf01c2ac0e2d04b
caixiongjiang <1837278011@qq.com>
2021-09-22 09:35:10 +0800

    master

--- test/test4.cpp
+++ test/test4.cpp
@@ -1,30 +1,57 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-//引用做函数的返回值
-//1.不要返回局部变量的引用
-int& test01() {
-	int a = 10;//局部变量存放在4区中的栈区	
-	return a;
+//类对象作为类成员
+
+//手机类
+class Phone {
+public:
+	Phone(string pName) {
+		cout << "Phone的构造函数调用" << endl;
+		m_PName = pName;
+	}
+
+	~Phone() {
+		cout << "Phone的析构函数调用" << endl;
+	}
+
+	//手机品牌的名称
+	string m_PName;
+};
+
+//人类
+class	 Person {
+public:
+	//Phone m_Phone = pName;   隐式转化法
+	Person(string name, string pName) : m_Name(name), m_Phone(pName){
+		cout << "Person的构造函数调用" << endl;
+	}
+
+	~Person(){
+		cout << "Person的析构函数调用" << endl;
+	}
+
+	//姓名
+	string m_Name;
+	//手机
+	Phone m_Phone;
+};
+
+//当其他的类的对象作为本类的成员，构造时候先构造类对象，再构造自身。（先构造腿才能去构造身体）
+//析构的顺序与构造相反（先把身体拆了才能拆腿）
+
+void test01() {
+	Person p("张三", "苹果MAX");
+	cout << p.m_Name << "拿着：" << p.m_Phone.m_PName << endl;
 }
 
-//2.函数的调用可以作为左值
-int& test02() {
-	static int a = 10;//静态变量存放在全局区，数据在程序结束后由系统释放
-	return a;
-}
 
 int main() {
+	test01();
 
-	int& ref = test01();
-	cout << "ref = " << ref << endl;//第一次结果正确为10是因为编译器做了保留
-	cout << "ref = " << ref << endl;//第二次结果错误，因为a的内存已被释放。
 
-	int& res = test02();
-	cout << "res = " << res << endl;//10
-	test02() = 1000;//如果函数的返回值是引用，函数的返回值可以作为左值
-	cout << "res = " << res << endl;//1000
 
 	system("pause");
 	return 0;
