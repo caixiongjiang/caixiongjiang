c32b8594df0d932d4730bac92dcb714acde36037
nau_cxj <1837278011@qq.com>
2021-10-06 10:57:14 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -3,17 +3,12 @@
 
 using namespace std;
 
-//建筑物类
-class Building 
-{
-	//goodGay全局函数是 Building好朋友，可以访问Building中私有的成员
-	friend void goodGay(Building* building);
+//类做友元
+class Building {
+	//GoodGay是本类的好朋友，可以访问本类中私有的成员
+	friend class GoodGay;
 public:
-	Building()
-	{
-		m_sittingRoom = "客厅";
-		m_BedRoom = "卧室";
-	}
+	Building();
 
 public:
 	string m_sittingRoom; // 客厅
@@ -22,23 +17,41 @@ private:
 	string m_BedRoom; //卧室
 };
 
-//全局函数
-void goodGay(Building* building)
-{
-	cout << "好基友全局函数 正在访问：" << building->m_sittingRoom << endl;
-
-	cout << "好基友全局函数 正在访问：" << building->m_BedRoom << endl;
+//类外写成员函数
+Building::Building(){
+	m_sittingRoom = "客厅";
+	m_BedRoom = "卧室";
 }
 
-void test01()
+class GoodGay {
+public:
+
+	GoodGay();
+
+	void visit(); //参观函数访问Building中的属性
+
+	Building* building;
+
+};
+
+GoodGay::GoodGay()
 {
-	Building building;
-	goodGay(&building);
+	//创建建筑物对象
+	building = new Building;
 }
 
+void GoodGay::visit() {
+	cout << "好基友类正在访问：" << building->m_sittingRoom << endl;
+
+	cout << "好基友类正在访问：" << building->m_BedRoom << endl;
+}
+
+void test01() {
+	GoodGay gg;
+	gg.visit();
+}
 
 int main() {
-
 	test01();
 	system("pause");
 	return 0;
