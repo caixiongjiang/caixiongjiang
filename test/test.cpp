8076f6f24be7a4ef7cded67ab447cca2fb9228ca
nau_cxj <1837278011@qq.com>
2021-10-06 10:29:36 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -1,105 +1,45 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-//1.创建一个立方体的类
-//2.设计属性和行为
-//3.获取立方体的面积和体积
-//4.分别利用全局函数和成员函数判断两个立方体是否相等
-class Cube {
+//建筑物类
+class Building 
+{
+	//goodGay全局函数是 Building好朋友，可以访问Building中私有的成员
+	friend void goodGay(Building* building);
 public:
-	void setL(int l) {
-		m_L = l;
+	Building()
+	{
+		m_sittingRoom = "客厅";
+		m_BedRoom = "卧室";
 	}
 
-	int getL() {
-		return m_L;
-	}
+public:
+	string m_sittingRoom; // 客厅
 
-	void setW(int w) {
-		m_W = w;
-	}
-	
-	int getW() {
-		return m_W;
-	}
-
-	void setH(int h) {
-		m_H = h;
-	}
-	
-	int getH() {
-		return m_H;
-	}
-//获取立方体面积
-	int calculateS() {
-		return 2 * m_L * m_W + 2 * m_W * m_H + 2 * m_L * m_H;
-	}
-//获取立方体体积
-	int calculateV() {
-		return m_L * m_W * m_H;
-	}
-//利用成员函数判断两个立方体是否相等
-	bool isSameByClass(Cube &c) {
-		if (m_L == c.getL() && m_W == c.getW() && m_H == c.getH()) {
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
 private:
-	
-	int m_L;//长
-	int m_W;//宽
-	int m_H;//高
+	string m_BedRoom; //卧室
 };
 
-//利用全局函数判断两个立方体是否相等
-bool isSame(Cube &c1, Cube &c2) {
-	if (c1.getL() == c2.getL() && c1.getW() == c2.getW() && c1.getH() == c2.getH()) {
-		return true;
-	}
-	else {
-		return false;
-	}
+//全局函数
+void goodGay(Building* building)
+{
+	cout << "好基友全局函数 正在访问：" << building->m_sittingRoom << endl;
+
+	cout << "好基友全局函数 正在访问：" << building->m_BedRoom << endl;
 }
 
+void test01()
+{
+	Building building;
+	goodGay(&building);
+}
+
+
 int main() {
-	//创建一个立方体
-	Cube c1;
-	c1.setL(10);
-	c1.setW(20);
-	c1.setH(20);
 
-	cout << "c1的面积为：" << c1.calculateS() << endl;
-	cout << "c1的体积为：" << c1.calculateV() << endl;
-
-	//创建第二个立方体
-	Cube c2;
-	c2.setL(11);
-	c2.setW(20);
-	c2.setH(20);
-	
-	//利用全局函数判断立方体是否相等
-	bool ret = isSame(c1, c2);
-	if (ret) {
-		cout << "c1和c2是相等的" << endl;
-	}
-	else
-	{
-		cout << "c1和c2不相等" << endl;
-	}
-	//利用成员函数判断立方体是否相等
-	bool ret2 = c1.isSameByClass(c2);
-	if (ret2) {
-		cout << "c1和c2是相等的" << endl;
-	}
-	else
-	{
-		cout << "c1和c2不相等" << endl;
-	}
+	test01();
 	system("pause");
 	return 0;
 }
