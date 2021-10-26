f29ee2bca0e0aa2290056ddb5632e59ac5051275
nau_cxj <1837278011@qq.com>
2021-10-26 12:52:32 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -2,57 +2,36 @@
 
 using namespace std;
 
-//关系运算符重载
-class Person {
+//动物类
+class Animal 
+{
 public:
-	Person(string name, int age) {
-		m_Name = name;
-		m_Age = age;
-	}
-	//重载关系运算符 == 号
-	bool operator==(Person& p) {
-		if (this->m_Name == p.m_Name && this->m_Age == p.m_Age) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	//重载关系运算符 !=号
-	bool operator!=(Person& p) {
-		if (this->m_Name != p.m_Name || this->m_Age != p.m_Age) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	string m_Name;
 	int m_Age;
-
 };
+//利用虚继承 解决菱形继承的问题
+// 继承之前 加上关键字 virtual 变为虚继承
+// Animal类称为  虚基类
+//羊类
+class Sheep :virtual public Animal{};
 
-void test01(){
-	Person p1("Tom", 18);
-	Person p2("Jerry", 18);
-	
-	if (p1 == p2) {
-		cout << "p1和p2是相等的" << endl;
-	}
-	else {
-		cout << "p1和p2是不相等的" << endl;
-	}
+//驼类
+class Tuo :virtual public Animal{};
+//羊驼类
+class SheepTuo :public Sheep, public Tuo{};
 
-	if (p1 != p2) {
-		cout << "p1和p2是不相等的" << endl;
-	}
-	else {
-		cout << "p1和p2是相等的" << endl;
-	}
+void test01() 
+{
+	SheepTuo st;
+	st.Sheep::m_Age = 18;
+	st.Tuo::m_Age = 28;
+	//当菱形继承，两个父类拥有相同的数据，需要加以作用域区分
+	cout << "st.Sheep::m_Age = " << st.Sheep::m_Age << endl;
+	cout << "st.Tuo::m_Age = " << st.Tuo::m_Age << endl;
+	cout << "st.m_Age = " << st.m_Age << endl;
+	//而羊驼的数据只需要一份就可以，菱形数据导致数据有两份，资源浪费
 }
-
-int main() {
+int main() 
+{
 	test01();
 	system("pause");
 	return 0;
