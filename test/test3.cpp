450455d463cb56cb9af07356b1c10a9bf6327209
nau_cxj <1837278011@qq.com>
2021-10-26 09:34:32 +0800

    master

--- test/test3.cpp
+++ test/test3.cpp
@@ -2,43 +2,132 @@
 
 using namespace std;
 
-//左移运算符重载
+//普通实现页面
 
-class Person {
-	friend ostream& operator<<(ostream& cout, Person& p);
+//Java页面
+//class Java {
+//public:
+//	void header() {
+//		cout << "首页，公开课，登录，注册...(公共头部)" << endl;
+//	}
+//	void footer() {
+//		cout << "帮助中心，交流合作，站内地图...(公共底部)" << endl;
+//	}
+//	void left() {
+//		cout << "Java, Python, C++,...(公共分类列表)" << endl;
+//	}
+//	void content() {
+//		cout << "Java 学科视频" << endl;
+//	}
+//};
+//
+////Python页面
+//class Python {
+//public:
+//	void header() {
+//		cout << "首页，公开课，登录，注册...(公共头部)" << endl;
+//	}
+//	void footer() {
+//		cout << "帮助中心，交流合作，站内地图...(公共底部)" << endl;
+//	}
+//	void left() {
+//		cout << "Java, Python, C++,...(公共分类列表)" << endl;
+//	}
+//	void content() {
+//		cout << "Python 学科视频" << endl;
+//	}
+//};
+////C++页面
+//class Cpp {
+//public:
+//	void header() {
+//		cout << "首页，公开课，登录，注册...(公共头部)" << endl;
+//	}
+//	void footer() {
+//		cout << "帮助中心，交流合作，站内地图...(公共底部)" << endl;
+//	}
+//	void left() {
+//		cout << "Java, Python, C++,...(公共分类列表)" << endl;
+//	}
+//	void content() {
+//		cout << "C++学科视频" << endl;
+//	}
+//};
+
+
+//继承实现页面
+
+//公共页面类
+class BasePage {
 public:
-	Person(int a, int b) {
-		m_A = a;
-		m_B = b;
+	void header() {
+		cout << "首页，公开课，登录，注册...(公共头部)" << endl;
 	}
-private:
-	//利用成员函数来重载左移运算符 p.operator(cout) 简化版本 p << cout
-	//不会利用成员函数重载<< 运算符，因为无法实现cout在左侧
-	//void operator << (cout) {
-	//
-	//}
-
-	int m_A;
-	int m_B;
+	void footer() {
+		cout << "帮助中心，交流合作，站内地图...(公共底部)" << endl;
+	}
+	void left() {
+		cout << "Java, Python, C++,...(公共分类列表)" << endl;
+	}
 };
 
-//只能利用全局函数来实现重载左移运算符
-ostream & operator<<(ostream &cout ,Person &p) {		//本质  operator<<(cout, p)    简化 cout << p
-	cout << "m_A =" << p.m_A << "  m_B =" << p.m_B;
-	return cout;
-}
+//继承的好处：减少重复代码
+//语法： class 子类: 继承方式 父类
+//子类  也称为  派生类
+//父类  也称为  基类
+
+//Java页面
+class Java : public BasePage {
+public:
+	void content() {
+		cout << "Java学科视频" << endl;
+	}
+};
+
+//Python页面
+class Python : public BasePage {
+public:
+	void content() {
+		cout << "Python学科视频" << endl;
+	}
+};
+
+//C++页面
+class Cpp : public BasePage {
+public:
+	void content() {
+		cout << "C++学科视频" << endl;
+	}
+};
+
 
 void test01() {
-	Person p(10, 10);
-	//p.m_A = 10;
-	//p.m_B = 10;
+	cout << "Java下载视频页面如下：" << endl;
+	Java ja;
+	ja.header();
+	ja.footer();
+	ja.left();
+	ja.content();
 
-	cout << p <<"  Hello World!" <<endl;
+	cout << "---------------------------" << endl;
+	cout << "Python下载视频页面如下：" << endl;
+	Python py;
+	py.header();
+	py.footer();
+	py.left();
+	py.content();
+
+	cout << "---------------------------" << endl;
+	cout << "C++下载视频页面如下：" << endl;
+	Cpp cpp;
+	cpp.header();
+	cpp.footer();
+	cpp.left();
+	cpp.content();
 }
 
 int main() {
 	test01();
-
 	system("pause");
 	return 0;
 }
