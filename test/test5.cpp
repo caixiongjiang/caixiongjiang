bc29a71d420df88fc9c154d8b3d8b89583ca195b
nau_cxj <1837278011@qq.com>
2021-10-26 11:18:10 +0800

    master

--- test/test5.cpp
+++ test/test5.cpp
@@ -1,50 +1,58 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-//重载递减运算符
-
-//自定义整型
-class Myinteger {
-	friend ostream& operator<<(ostream& cout, Myinteger myint);
+//继承中同名成员处理
+class Base
+{
 public:
-	Myinteger() {
-		m_Num = 3;
+	Base() 
+	{
+		m_A = 100;
 	}
-	//重载前置--运算符  返回引用为了一直对一个数据进行递减操作
-	Myinteger& operator--() {
-		//先进行--的运算，再将自身做返回
-		m_Num--;
-		return *this;
+	void func()
+	{
+		cout << "Base-func()函数调用" << endl;
 	}
-	//重载前置--运算符  返回值
-	Myinteger operator--(int) {
-		//先记录当时的结果
-		Myinteger temp = *this;
-		//后递增
-		m_Num--;
-		//最后将记录结果返回
-		return temp;
+	void func(int a)
+	{
+		cout << "Base-func(int a)函数调用" << endl;
 	}
-private:
-	int m_Num;
+	int m_A;
 };
 
-ostream& operator<<(ostream& cout, Myinteger myint) {
-	cout << myint.m_Num;
-	return cout;
+class Son :public Base
+{
+public:
+	Son()
+	{
+		m_A = 200;
+	}
+	void func()
+	{
+		cout << "Son-func()函数调用" << endl;
+	}
+	int m_A;
+};
+
+void test01()
+{
+	Son s;
+	cout << "Son下的m_A = " << s.m_A << endl;	//输出200
+	//如果通过子类对象 访问父类中的同名成员， 需要加一个作用域
+	cout << "Base下的m_A = " << s.Base::m_A << endl;	//输出100
 }
 
-void test01() {
-	Myinteger myint;
-	cout << --(--myint) << endl;
-	cout << myint << endl;
-}
-
-void test02() {
-	Myinteger myint;
-	cout << myint-- << endl;
-	cout << myint << endl;
+//同名成员函数的处理方式
+void test02() 
+{
+	Son s;
+	s.func();//直接调用 调用的是子类中的同名成员
+	//如何调用到父类中的同名成员函数？
+	s.Base::func();
+	//如果子类中出现和父类同名的成员函数，子类的同名成员会隐藏掉父类的所有同名成员函数
+	//s.func(100);   出现报错
+	s.Base::func(100);
 }
 
 int main() {
