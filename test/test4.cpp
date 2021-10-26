916044fe2f61c1f3e4d10e9ba3cd8c9d0f51bfc8
nau_cxj <1837278011@qq.com>
2021-10-26 10:10:43 +0800

    master

--- test/test4.cpp
+++ test/test4.cpp
@@ -1,60 +1,37 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-//重载递增运算符
-
-//自定义整型
-class MyInteger
-{
-friend ostream& operator<<(ostream& cout, MyInteger myint);
+//继承中的对象模型
+class Base {
 public:
-	MyInteger() {
-		m_Num = 0;
-	}
-	
-	//重载前置++运算符    返回引用为了一直对一个数据进行递增操作
-	MyInteger& operator++() {
-		//先进行++的运算，再将自身做一个返回
-		m_Num++;
-		return *this; 
-	}
-	//重载后置++运算符
-	//MyInteger& operator++(int)   int代表占位参数，可以用于区分前置和后置递增
-	MyInteger operator++(int) {
-		//先记录当时的结果
-		MyInteger temp = *this;
-		//后递增
-		m_Num++;
-		//最后将记录结果做返回
-		return temp;
-	}
+	int m_A;
+protected:
+	int m_B;
 private:
-	int m_Num;
+	int m_C;
 };
 
-//重载<<运算符
-ostream& operator<<(ostream& cout, MyInteger myint) {
-	cout << myint.m_Num;
-	return cout;
-}
+class Son :public Base {
+public:
+	int m_D;
+};
 
+//利用开发人员命令提示工具查看对象模型
+//跳转盘符 F：
+//跳转文件路径 cd 具体路径下
+//查看命名（dir）
+//cl /d1 reportSingleClassLayout类名 文件名 
 
 void test01() {
-	MyInteger myint;
-	cout << ++(++myint) << endl;
-	cout << myint << endl;
-}
-
-void test02() {
-	MyInteger myint;
-	cout << myint++ << endl;
-	cout << myint << endl;
+	//结果为16
+	//父类中所有非静态成员属性都会被子类继承下去
+	//父类中私有的成员属性 是被编译器给隐藏了，因此访问不到，但是确实被继承下去了
+	cout << "size of Son = " << sizeof(Son) << endl;
 }
 
 int main() {
 	test01();
-	test02();
 	system("pause");
 	return 0;
 }
