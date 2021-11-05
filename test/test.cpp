b67b85d182e1daf14b8b4663478ac3ad50b5e2a3
nau_cxj <1837278011@qq.com>
2021-11-05 19:40:10 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -1,66 +1,31 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
-//多态
-//动物类
-class Animal 
+//文本文件中 写文件
+
+void test01()
 {
-public:
-	//虚函数
-	virtual void speak()
-	{
-		cout << "动物在说话" << endl;
-	}
-};
+	//1.包含头文件 fstream
 
-//猫类
-class Cat:public Animal 
-{
-public:
-	//重写 ： 函数返回值类型 函数名 参数列表 完全相同
-	void speak() 
-	{
-		cout << "小猫在说话" << endl;
-	}
-};
+	//2.创建流对象
+	ofstream ofs;
+	
+	//3.指定打开方式
+	ofs.open("test.txt", ios::out);
 
-//狗类
-class Dog :public Animal
-{
-public:
-	void speak()
-	{
-		cout << "小狗在说话" << endl;
-	}
-};
-
-
-//执行说话的函数
-//地址早绑定  在编译阶段就确定了函数的地址
-//如果想执行让猫说话，那么这个函数地址就不能提前绑定，需要在运行阶段进行绑定，地址晚绑定
-
-//动态多态满足条件
-//1.有继承关系
-//2.子类要重写父类的虚函数
-
-//动态多态的使用
-//父类的指针或者引用   执行子类对象
-
-void doSpeak(Animal &animal) //Animal &animal = cat
-{
-	animal.speak();
+	//4.写内容
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：18" << endl;
+	
+	//5.关闭文件
+	ofs.close();
 }
 
-void test01() 
+int main()
 {
-	Cat cat;
-	doSpeak(cat);
-	Dog dog;
-	doSpeak(dog);
-}
-
-int main() {
 	test01();
 	system("pause");
 	return 0;
