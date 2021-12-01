5effd6243e1a1a59cf6786aa26e0e3ffaedfe4af
caixiongjiang <1837278011@qq.com>
2021-12-01 11:21:39 +0800

    master

--- test/test.cpp
+++ test/test.cpp
@@ -1,32 +1,53 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+using namespace  std;
 
-using namespace std;
+//函数模版
 
-//文本文件中 写文件
-
-void test01()
+//交换两个整数的函数
+void swapInt(int &a, int&b)
 {
-	//1.包含头文件 fstream
-
-	//2.创建流对象
-	ofstream ofs;
-	
-	//3.指定打开方式
-	ofs.open("test.txt", ios::out);
-
-	//4.写内容
-	ofs << "姓名：张三" << endl;
-	ofs << "性别：男" << endl;
-	ofs << "年龄：18" << endl;
-	
-	//5.关闭文件
-	ofs.close();
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
-int main()
+//交换两个浮点型的函数
+void swapDouble(double &a, double &b)
 {
-	test01();
-	system("pause");
-	return 0;
+    double temp = a;
+    a = b;
+    b = temp;
+}
+//函数模版
+template<typename  T>   //声明一个模版，告诉编译器后面代码中紧跟着的T不要报错，T是一个用用数据类型
+void mySwap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+void test01()
+{
+    int a = 10;
+    int b = 20;
+    //swapInt(a, b);
+    //利用函数模版交换
+    //两种方式使用函数模版
+    //1.自动类型推倒
+    //mySwap(a, b);
+    //2.显示指定类型
+    mySwap<int>(a, b);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    double c = 1.1;
+    double d = 2.2;
+    swapDouble(c, d);
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+}
+
+int main() {
+    test01();
+    return 0;
 }
